6ddd0486553604fa629cd0df9ac4a84b1e8ac347
Lewis Storey (BunnyRabbit2) <iamlewisstorey@yahoo.com>
2018-07-25 22:46:31 +0100

    done for tonight. will be recreating the rest some other time
    Signed-off-by: Lewis Storey (BunnyRabbit2) <iamlewisstorey@yahoo.com>

--- index.php
+++ index.php
@@ -4,38 +4,80 @@
 <head>
   <title>My custom Bulma website</title>
   <link rel="stylesheet" href="css/mystyles.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 </head>
 
-<body>
-  <h1 class="title">
-    Bulma
-  </h1>
-
-  <p class="subtitle">
-    Modern CSS framework based on
-    <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">Flexbox</a>
-  </p>
-
-  <div class="field">
-    <div class="control">
-      <input class="input" type="text" placeholder="Input">
+<body class="has-navbar-fixed-top">
+  <nav class="navbar is-fixed-top is-info">
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="#page-top">
+          BUNNYRABBIT2.COM
+        </a>
+        <span class="navbar-burger burger" data-target="navbarMenuHeroA">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+      </div>
+      <div id="navbarMenuHeroA" class="navbar-menu">
+        <div class="navbar-end">
+          <a class="navbar-item">
+            Portfolio
+          </a>
+          <a class="navbar-item">
+            About
+          </a>
+          <a class="navbar-item">
+            Contact
+          </a>
+        </div>
+      </div>
     </div>
-  </div>
+  </nav>
+  <section class="hero is-primary">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <div class="level">
+          <div class="level-item has-text-centered">
+            <figure class='image is-128x128'>
+              <img src='https://bulma.io/images/placeholders/128x128.png'>
+            </figure>
+          </div>
+        </div>
+        <h1>BUNNYRABBIT2</h1>
+        <hr></hr>
+        <h2>Coder - Geek - Gamer</h2>
+      </div>
+    </div>
+  </section>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <div class="level">
 
-  <div class="field">
-    <p class="control">
-      <span class="select">
-        <select>
-          <option>Select dropdown</option>
-        </select>
-      </span>
-    </p>
-  </div>
-
-  <div class="buttons">
-    <a class="button is-primary">Primary</a>
-    <a class="button is-link">Link</a>
-  </div>
+        <div class="level-item">
+          <h3>Location</h3>
+          <p>Thanet
+            <br>United Kingdom</p>
+        </div>
+        <div class="level-item">
+          <h3>Around the Web</h3>
+          <div class="level">
+            <div class="level-item">
+              <a href="http://twitter.com/bunnyrabbit2">
+                <i class="fab fa-twitter fa-4x"></i>
+              </a>
+            </div>
+            <div class="level-item">
+              <a href="https://github.com/BunnyRabbit2">
+                <i class="fab fa-github fa-4x"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
 </body>
 
 </html>
