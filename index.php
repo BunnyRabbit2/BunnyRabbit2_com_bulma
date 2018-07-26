739c4f83166b51cfb0263744a598db31ed9057f6
Lewis Storey (BunnyRabbit2) <iamlewisstorey@yahoo.com>
2018-07-26 22:36:33 +0100

    Profile Section completed. About to break the site into multiple files
    Signed-off-by: Lewis Storey (BunnyRabbit2) <iamlewisstorey@yahoo.com>

--- index.php
+++ index.php
@@ -40,14 +40,14 @@
       <div class="container has-text-centered">
         <div class="level">
           <div class="level-item has-text-centered">
-            <figure class='image is-128x128'>
-              <img src='https://bulma.io/images/placeholders/128x128.png'>
+            <figure class='image is-256x256'>
+              <img class="profile-pic" src='/img/profile_pic.jpg'>
             </figure>
           </div>
         </div>
-        <h1>BUNNYRABBIT2</h1>
-        <hr></hr>
-        <h2>Coder - Geek - Gamer</h2>
+        <h1 class="is-size-1 is-uppercase has-text-weight-bold">BunnyRabbit2</h1>
+        <hr class="name-divider"></hr>
+        <h2 class="is-size-4">Coder - Geek - Gamer</h2>
       </div>
     </div>
   </section>
