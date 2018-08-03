79941827d0c297f413a73263430cef1deada6b49
Lewis Storey (BunnyRabbit2) <iamlewisstorey@yahoo.com>
2018-08-04 00:45:17 +0100

    Added made with bulma to footer
    Signed-off-by: Lewis Storey (BunnyRabbit2) <iamlewisstorey@yahoo.com>

--- footer.php
+++ footer.php
@@ -32,5 +32,12 @@
         </p>
       </div>
     </div>
+    <div class="columns is-centred">
+      <div class="column has-text-centered">
+        <a href="https://bulma.io">
+          <img src="https://bulma.io/images/made-with-bulma--white.png" alt="Made with Bulma" width="128" height="24">
+        </a>
+      </div>
+    </div>
   </div>
 </footer>
