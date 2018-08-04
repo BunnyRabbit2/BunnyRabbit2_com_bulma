200f7b9218e34cc9a81bfb5c1e460b9cb8fb8acc
Lewis Storey (BunnyRabbit2) <iamlewisstorey@yahoo.com>
2018-08-04 01:24:36 +0100

    Built contact page. Need to sort out validation and sending
    Signed-off-by: Lewis Storey (BunnyRabbit2) <iamlewisstorey@yahoo.com>

--- contact.php
+++ contact.php
@@ -1,4 +1,51 @@
-<!-- Portfolio -->
-<section>
-    
+<!-- Contact -->
+<section id="contact" class="hero is-bold hero-extra-padding">
+    <div class="hero-body">
+        <div class=" columns">
+            <div class="column has-text-centered">
+                <h1 class="title is-1 is-uppercase">Contact Me</h1>
+                <hr class="page-divider">
+            </div>
+        </div>
+        <div class="columns is-centered">
+            <div class="column is-half">
+                <div class="field">
+                    <div class="control has-icons-left has-icons-right">
+                        <input class="input is-large" type="text" placeholder="Name">
+                        <span class="icon is-medium is-left">
+                            <i class="fas fa-user fa-lg"></i>
+                        </span>
+                    </div>
+                </div>
+                <div class="field">
+                    <div class="control has-icons-left has-icons-right">
+                        <input class="input is-large" type="email" placeholder="Email Address">
+                        <span class="icon is-medium is-left">
+                            <i class="fas fa-at fa-lg"></i>
+                        </span>
+                    </div>
+                </div>
+                <div class="field">
+                    <div class="control has-icons-left has-icons-right">
+                        <input class="input is-large" type="text" placeholder="Phone Number">
+                        <span class="icon is-medium is-left">
+                            <i class="fas fa-phone fa-lg"></i>
+                        </span>
+                    </div>
+                </div>
+                <div class="field">
+                    <div class="control has-icons-left has-icons-right">
+                        <textarea class="textarea is-large" type="text" placeholder="Message"></textarea>
+                        <span class="icon is-medium is-left">
+                            <i class="fas fa-comment-alt fa-lg"></i>
+                        </span>
+                    </div>
+                </div>
+                <div class="field is-grouped">
+                    <div class="control">
+                        <button class="button is-info">Send Message</button>
+                    </div>
+                </div>
+            </div>
+        </div>
 </section>
